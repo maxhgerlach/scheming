9e6482aec945e32c7ffe192aa0869b6db3ce814c
Max H. Gerlach <m@maxgerlach.de>
2018-02-17 20:01:14 +0100

    ch. 1.2 going on

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -136,3 +136,21 @@
     (< (abs (- (square guess) x)) 0.001))
   (sqrt-iter 1.0))
 (sqrt 15)
+
+
+;; Chap. 1.2
+(define (factorial n)
+  (if (> n 1)
+      (* n (factorial (- n 1)))
+      1))
+(factorial 6)
+
+(define (factorial n)
+  (define (fact-iter product counter max-count)
+    (if (> counter max-count)
+        product
+        (fact-iter (* counter product)
+                   (+ counter 1)
+                   max-count)))
+  (fact-iter 1 1 n))
+(factorial 6)
