10061f741ee661b6fc7683e146c58cb3a76110ba
Max H. Gerlach <m@maxgerlach.de>
2018-02-17 13:58:32 +0100

    compile with guile

--- guile-tutorial/tortoise.c
+++ guile-tutorial/tortoise.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
+#include <libguile.h>
 
 static const int WIDTH = 10;
 static const int HEIGHT = 10;
@@ -98,21 +99,20 @@ tortoise_move (double length)
     y = newY;
 }
 
+static void*
+register_functions (void* data)
+{
+    return NULL;
+}
+
 int
 main (int argc, char* argv[])
 {
     global_output = start_gnuplot ();
     tortoise_reset();
 
-    {
-        int i;
-        tortoise_pendown (); /* This is unnecessary, but makes it clearer.  */
-        for (i = 1; i <= 4; ++i)
-        {
-            tortoise_move (3.0);
-            tortoise_turn (90.0);
-        }
-    }
+    scm_with_guile(&register_functions, NULL);
+    scm_shell(argc, argv);
 
     return EXIT_SUCCESS;
 }
