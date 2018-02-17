523c81e2cb1afbf825fbf5ba54bc537ab071bc68
Max H. Gerlach <m@maxgerlach.de>
2018-02-17 13:38:50 +0100

    guile-tutorial: tortoise.c

---
+++ guile-tutorial/tortoise.c
@@ -0,0 +1,49 @@
+/* Simple backend for a Logo like tortoise drawer.  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static const int WIDTH = 10;
+static const int HEIGHT = 10;
+
+static FILE*
+start_gnuplot ()
+{
+    FILE* output;
+    int pipes[2];
+    pid_t pid;
+
+    pipe (pipes);
+    pid = fork ();
+
+    if (!pid)
+    {
+        dup2 (pipes[0], STDIN_FILENO);
+        execlp ("gnuplot", NULL);
+        return; /* Not reached.  */
+    }
+
+    output = fdopen (pipes[1], "w");
+
+    fprintf (output, "set multiplot\n");
+    fprintf (output, "set parametric\n");
+    fprintf (output, "set xrange [-%d:%d]\n", WIDTH, WIDTH);
+    fprintf (output, "set yrange [-%d:%d]\n", HEIGHT, HEIGHT);
+    fprintf (output, "set size ratio -1\n");
+    fprintf (output, "unset xtics\n");
+    fprintf (output, "unset ytics\n");
+    fflush (output);
+
+    return output;
+}
+
+static FILE* global_output;
+
+int
+main (int argc, char* argv[])
+{
+    global_output = start_gnuplot ();
+
+    return EXIT_SUCCESS;
+}
