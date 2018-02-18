44caca0336eb39c7c0a6795e6e09e41e24f4bdb3
Max H. Gerlach <m@maxgerlach.de>
2018-02-18 13:53:29 +0100

    ex 1.20

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -362,3 +362,74 @@
                           q
                           (- count 1)))))
   (fib-iter 1 0 0 1 n))
+
+
+;; 1.2.5 Greatest Common Divisors
+
+;; euclids algorithm
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+;; Exercise 1.20
+;;
+;; normal order, directly expanding (if):
+(gcd 206 40)
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+(gcd 40 (remainder 206 40))
+(if (= (remainder 206 40) 0)            ;* 1
+    a
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+(if (= (remainder 40 (remainder 206 40)) 0) ;** 2
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) ;**** 4
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0) ;******* 7
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+(remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ;**** 4
+2
+;;
+;; 1 + 2 + 4 + 7 + 4 = 18 remainder operations
+;;
+;; applicative order
+(gcd 206 40)
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+(gcd 40 (remainder 206 40))             ;* 1
+(gcd 40 6)
+(if (= 6 0)
+    40
+    (gcd 6 (remainder 40 6)))
+(gcd 6 (remainder 40 6))                ;* 1
+(gcd 6 4)
+(if (= 4 0)
+    6
+    (gcd 4 (remainder 6 4)))
+(gcd 4 (remainder 6 4))                 ;* 1
+(gcd 4 2)
+(if (= 2 0)
+    4
+    (gcd 2 (remainder 4 2)))
+(gcd 2 (remainder 4 2))                 ;* 1
+(gcd 2 0)
+(if (= 0 0)
+    2
+    (gcd 2 (remainder 2 0)))
+2
+;;
+;; 4 remainder operations
