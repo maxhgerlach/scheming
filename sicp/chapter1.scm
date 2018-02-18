14e73dd7ece11acda3e12f3e2fff8e053aad07a0
Max H. Gerlach <m@maxgerlach.de>
2018-02-18 13:01:21 +0100

    ex 1.19

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -344,3 +344,21 @@
           ((not (even? b)) (*iter (+ x a) a (- b 1)))
           (else (*iter x (double a) (halve b)))))
   (*iter 0 a b))
+
+;; Exercise 1.19 : Fibonacci in logarithmic time
+;; T^2_{p,q} = T_{p',q'} with p' = pp+qq, q' = qq+2pq
+(define (fast-fib n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (* p p) (* q q))         ; compute p ′
+                     (+ (* q q) (* 2 p q))      ; compute q ′
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
