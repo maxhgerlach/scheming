8f3ffc5314cc769b77f97f6f0697457864b62c60
Max H. Gerlach <m@maxgerlach.de>
2018-02-20 21:17:03 +0100

    ex. 1.36

--- sicp/chapter1.scm
+++ sicp/chapter1.scm
@@ -785,3 +785,72 @@
 ;(f f)
 ;(f 2)
 ;(2 2) ; error
+
+
+
+;; 1.3.3 Procedures as General Methods
+;;
+;;
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (close-enough? x y) (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+
+;; Exercise 1.35
+;;
+;; golden ratio phi^2 = phi + 1 -> phi = 1 + 1/phi
+
+(fixed-point (lambda (x) (+ 1. (/ 1. x))) 1.0) ; 1.6180327868852458
+
+
+;; Exercise 1.36
+
+(define (verbose-fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; (verbose-fixed-point (lambda (x) (+ 1. (/ 1. x))) 1.0)
+
+(verbose-fixed-point (lambda (x) (/ (log 1000) (log x))) 1.5) ;35 steps until 4.555539351985717
+
+(verbose-fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 1.5) ;11 steps until 4.555537952796512
