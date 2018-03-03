7c273f27e912a5469e6d936174b940ade666db25
Max H. Gerlach <m@maxgerlach.de>
2018-03-03 23:45:02 +0100

    end of 2.1

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -220,3 +220,22 @@
        (make-interval (/ 1.0 (upper-bound y))
                       (/ 1.0 (lower-bound y))))))
 
+
+;; skip ex. 2.11
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;; Exercise 2.12
+(define (make-center-percent c p)
+  (let ((w (abs (* c (/ p 100)))))
+    (make-center-width c w)))
+(define (percent i)
+  (* 100 (abs (/ (width i) (center i)))))
+
+
+;; skipping ex. 2.13...16
