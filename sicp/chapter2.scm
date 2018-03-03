2810402d8ae9cae9d33db50a00eae873a433f085
Max H. Gerlach <m@maxgerlach.de>
2018-03-03 16:54:31 +0100

    ex. 2.4

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -83,3 +83,25 @@
   (display (y-point p))
   (display ")")
   (newline))
+
+
+;; Exercise 2.3 (rectangles) skipped
+
+
+;; Exercise 2.4
+(define (my-cons x y)
+  (lambda (m) (m x y)))
+(define (my-car z)
+  (z (lambda (p q) p)))
+
+;; (my-car (my-cons 1 2))
+;; (my-car (lambda (m) (m 1 2)))
+;; ((lambda (m) (m 1 2)) (lambda (p q) p))
+;; ((lambda (p q) p) 1 2)
+;; 1
+
+(define (my-cdr z)
+  (z (lambda (p q) q)))
+
+;; (my-cdr (my-cons 1 2))
+;; 2
