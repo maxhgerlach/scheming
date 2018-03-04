aac9e46b77851b13270a5d35a702554f3239ea1a
Max H. Gerlach <m@maxgerlach.de>
2018-03-05 00:07:45 +0100

    ex 2.33

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -507,3 +507,20 @@
                (lambda (rest-el)
                  (cons (car s) rest-el))
                rest)))))
+
+
+;; Exercise 2.33
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map-via-acc p sequence)
+  (accumulate
+   (lambda (x y) (cons (p x) y))
+   '() sequence))
+(define (append-via-acc seq1 seq2)
+  (accumulate cons seq2 seq1))
+(define (length-via-acc sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
