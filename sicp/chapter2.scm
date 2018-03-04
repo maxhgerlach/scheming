3711c2c1d4ca65fd641b92386cfcd5605e5cb6a7
Max H. Gerlach <m@maxgerlach.de>
2018-03-05 00:14:24 +0100

    ex 2.34

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -524,3 +524,13 @@
   (accumulate cons seq2 seq1))
 (define (length-via-acc sequence)
   (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+
+;; Exercise 2.34
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+;; (horner-eval 2 (list 1 3 0 5 0 1))      ;79
