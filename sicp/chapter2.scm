cea8bc661812161ee7890f62970ac7650334a27f
Max H. Gerlach <m@maxgerlach.de>
2018-08-15 22:51:41 +0200

    ex. 2.59

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -958,3 +958,43 @@
 
 
 ;; example Ex. 2.58 part a in separate file, skipping part b for now
+
+
+;; 2.3.3 Example: Representing Sets
+;;
+;; Sets as unordered lists
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;; example
+(let* ((s (adjoin-set 'a '()))
+       (ss (adjoin-set 'b s)))
+  (element-of-set? 'b ss))              ; #t
+
+(intersection-set '(1 2 3 4 5 8) '(9 10 3 4 2)) ; (2 3 4)
+
+
+;; Exercise 2.59
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        (else (union-set (cdr set1) (adjoin-set (car set1) set2)))))
+
+(union-set '(1 2 3 4 5 8) '(9 10 3 4 2)) ; => (8 5 1 9 10 3 4 2)
+(union-set '(1 2 3 4 5 8) '())           ; => (8 5 4 3 2 1)
+(union-set '() '(1 2 3 4 5 8))           ; => (1 2 3 4 5 8)
+(union-set '() '())                      ; => ()
