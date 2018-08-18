b6a7393a2bd7b8fb75141acc6f3a2dec373b3599
Max H. Gerlach <m@maxgerlach.de>
2018-08-18 12:58:10 +0200

    ex. 2.63

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1112,3 +1112,95 @@
 (ord-union-set '(1 2 3 4 5 8) '())           ; => (8 5 4 3 2 1)
 (ord-union-set '() '(1 2 3 4 5 8))           ; => (1 2 3 4 5 8)
 (ord-union-set '() '())                      ; => ()
+
+
+
+;; Sets as binary trees
+;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set) (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+
+;; Exercise 2.63
+
+;; Trees from figure 2.16 (representing the set {1,3,5,7,9,11}
+
+(define t1 '(7 (3 (1 () ())
+                  (5 () ()))
+               (9 ()
+                  (11 () ()))))
+(define t2 '(3 (1 () ())
+               (7 (5 () ())
+                  (9 ()
+                     (11 () ())))))
+(define t3 '(5 (3 (1 () ())
+                  ())
+               (9 (7 () ())
+                  (11 () ()))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1
+                     (right-branch tree))))))
+
+(tree->list-1 t1)                       ; => (1 3 5 7 9 11)
+(tree->list-1 t2)                       ; => (1 3 5 7 9 11)
+(tree->list-1 t3)                       ; => (1 3 5 7 9 11)
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
+(tree->list-2 t1)                       ; => (1 3 5 7 9 11)
+(tree->list-2 t2)                       ; => (1 3 5 7 9 11)
+(tree->list-2 t3)                       ; => (1 3 5 7 9 11)
+
+
+;; Ex. 2.63.a
+;;
+;; tree->list-1 and tree->list-2 yield the same results (both traverse
+;; the tree in the same order)
+;;
+;;
+;;
+;; Ex. 2.63.b
+;;
+;; See handwritten calculations:
+;; 
+;; tree->list-1 is O(n log n) since append has linear complexity
+;;
+;; tree->list-2 is O(n) since cons has linear complexity
