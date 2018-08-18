b17f051d2931cf7f7c5558553915156b5a0c8e77
Max H. Gerlach <m@maxgerlach.de>
2018-08-18 14:00:49 +0200

    Ex. 2.66

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1280,3 +1280,14 @@
 (tree->list-2 '(3 (2 () ()) (4 () ())))                         ; (2 3 4)
 
 
+;; Ex. 2.66 -- set implemented as binary tree
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) #f)
+        ((= given-key (key (entry set-of-records)))
+         (entry set-of-records))
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup given-key (right-branch set-of-records)))))
+
