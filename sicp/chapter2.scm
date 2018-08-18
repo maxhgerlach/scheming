fc615db3c1fa2ea23ebe315d61efdc550a1758dc
Max H. Gerlach <m@maxgerlach.de>
2018-08-18 19:09:00 +0200

    Ex. 2.73

--- sicp/chapter2.scm
+++ sicp/chapter2.scm
@@ -1491,3 +1491,122 @@
 ;;
 ;; most frequent symbol: O(1)
 ;; least frequent symbol: O(n^2)
+
+
+
+;; 2.4 Multiple Representations for Abstract Data
+;;
+;;
+
+;; Exercise 2.73
+
+;; ;; original:
+;; (define (deriv exp var)
+;;   (cond ((number? exp) 0)
+;;         ((variable? exp)
+;;          (if (same-variable? exp var) 1 0))
+;;         ((sum? exp)
+;;          (make-sum (deriv (addend exp) var)
+;;                    (deriv (augend exp) var)))
+;;         ((product? exp)
+;;          (make-sum (make-product
+;;                     (multiplier exp)
+;;                     (deriv (multiplicand exp) var))
+;;                    (make-product
+;;                     (deriv (multiplier exp) var)
+;;                     (multiplicand exp))))
+;;         ;; ⟨ more rules can be added here ⟩
+;;         (else (error "unknown expression type: DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+
+;; data directed:
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp))
+               (operands exp) var))))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+
+;; a)
+;;
+;; number? and variable? directly check the type of their arguments,
+;; while the operator can be understood as a type tag for the other
+;; checks.
+
+
+
+;; b), c)
+(define (install-deriv-arithmetics-package)
+  (define (=number? exp num) (and (number? exp) (= exp num)))
+
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2))
+           (+ a1 a2))
+          (else (list '+ a1 a2))))
+
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2))))
+
+  (define (make-exponentiation b p)
+    (cond ((=number? p 0) 1)
+          ((=number? p 1) b)
+          ((and (number? b) (number? p))
+           (expt b p))
+          (else (list '** b p))))
+
+  (define (addend s) (car s))
+  (define (augend s) (accumulate make-sum 0 (cdr s)))
+
+  (define (multiplier p) (car p))
+  (define (multiplicand p) (accumulate make-product 1 (cdr p)))
+
+  (define (base e) (car e))
+  (define (exponent e) (cadr e))
+
+
+  ;; deriv
+  (define (deriv-sum s var)
+    (make-sum (deriv (addend exp) var)
+              (deriv (augend exp) var)))
+  (define (deriv-product p var)
+    (make-sum (make-product
+               (multiplier exp)
+               (deriv (multiplicand exp) var))
+              (make-product
+               (deriv (multiplier exp) var)
+               (multiplicand exp))))
+  (define (deriv-exponentiation e var)
+    (make-product (make-product (exponent exp)
+                                (make-exponentiation (base exp) (make-sum (exponent exp) (- 1))))
+                  (deriv (base exp) var)))
+
+  ;; install
+  (put 'deriv '+ deriv-sum)
+  (put 'deriv '* deriv-product)
+  (put 'deriv '** deriv-exponentiation)
+  )
+
+;; d
+;;
+;; For
+;; 
+;;       ((get (operator exp) 'deriv) (operands exp) var)
+;;       
+;; just reverse the order:
+;;
+;;  (put '+ 'deriv deriv-sum)
+;;  (put '* 'deriv deriv-product)
+;;  (put '** 'deriv deriv-exponentiation)
