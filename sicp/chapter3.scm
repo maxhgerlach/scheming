d2144be5bcf7347eff6b951920a01ac5239f2045
Max H. Gerlach <m@maxgerlach.de>
2018-08-26 16:20:45 +0200

    Ex. 3.2

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -18,3 +18,24 @@
 ;; scheme@(guile-user)> (A 10)
 ;; $11 = 8
 
+
+;; Ex. 3.2
+(define (make-monitored fn)
+  (let ((calls 0))
+    (define (dispatch m)
+      (if (eq? m 'how-many-calls?)
+          calls
+          (begin
+            (set! calls (+ calls 1))
+            (fn m))))
+    dispatch))
+
+;; scheme@(guile-user)> (define sqrt-mon (make-monitored sqrt))
+;; scheme@(guile-user)> (sqrt-mon 10.0)
+;; $13 = 3.1622776601683795
+;; scheme@(guile-user)> (sqrt-mon 100.0)
+;; $14 = 10.0
+;; scheme@(guile-user)> (sqrt-mon 101.0)
+;; $15 = 10.04987562112089
+;; scheme@(guile-user)> (sqrt-mon 'how-many-calls?)
+;; $16 = 3
