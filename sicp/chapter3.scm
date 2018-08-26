452ed62cfa02e14da79577e4bfc0b20d48d05e1d
Max H. Gerlach <m@maxgerlach.de>
2018-08-26 16:09:37 +0200

    Ex. 3.1

---
+++ sicp/chapter3.scm
@@ -0,0 +1,20 @@
+;; Chapter 3
+
+;; Ex. 3.1
+(define (make-accumulator sum)
+  (lambda (amount)
+    (begin (set! sum (+ sum amount))
+           sum)))
+
+;; scheme@(guile-user)> (define A (make-accumulator 0))
+;; scheme@(guile-user)> (A 0)
+;; $7 = 0
+;; scheme@(guile-user)> (A 1)
+;; $8 = 1
+;; scheme@(guile-user)> (A 2)
+;; $9 = 3
+;; scheme@(guile-user)> (A -5)
+;; $10 = -2
+;; scheme@(guile-user)> (A 10)
+;; $11 = 8
+
