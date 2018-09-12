80761f114c490a0fbdd45db9b7914bba44c06b20
Max H. Gerlach <m@maxgerlach.de>
2018-09-13 01:03:41 +0200

    ex. 3.21

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -338,3 +338,55 @@ n
 (contains-cycle-efficient problem-list-4) ; #t
 
 (contains-cycle-efficient '())          ; #f
+
+
+
+;; Chap. 3.3.2 Representing Queues
+
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
+
+
+;; Exercise 3.21
+
+(define (print-queue queue)
+  (define (iter lst)
+    (cond ((null? lst) '())
+          (else
+           (display (car lst))
+           (display " ")
+           (iter (cdr lst)))))
+  (iter (front-ptr queue))
+  (newline))
