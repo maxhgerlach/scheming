9b1ba07b48cbbbba7c07e74433fb890b757cda80
Max H. Gerlach <m@maxgerlach.de>
2018-09-16 00:26:58 +0200

    ex. 3.24

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -575,3 +575,47 @@ n
 ((example 'lookup-proc) 'math '/) ;#f
 ((example 'lookup-proc) 'math '-) ;45
 ((example 'lookup-proc) 'letters 'b)    ;98
+
+
+;; Exercise 3.24
+
+(define (make-table-test same-key?)
+  (define (assoc key records)
+    (cond ((null? records) #f)
+          ((same-key? key (caar records)) (car records))
+          (else (assoc key (cdr records)))))
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record (cdr record) #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define example2 (make-table-test (lambda (v1 v2) (< (abs (- v1 v2)) 0.1))))
+((example2 'insert-proc!) 0.1 9.9 10.0)
+((example2 'lookup-proc)  0.1 9.9)      ; 10.0
+((example2 'lookup-proc)  0.05 1.0)     ; #f
+((example2 'lookup-proc)  0.05 9.99999) ; 10.0
