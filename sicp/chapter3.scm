65fce9997f4964f632a91948356a6c35d74e051e
Max H. Gerlach <m@maxgerlach.de>
2018-09-16 00:18:23 +0200

    Representing Tables

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -524,3 +524,54 @@ n
             ((eq? m 'print) (print))
             (else (error "DEQUE does not know " m))))
     dispatch))
+
+
+;; 3.3.3 Representing Tables
+
+(define (assoc key records)
+  (cond ((null? records) #f)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record (cdr record) #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+
+(define example (make-table))
+((example 'insert-proc!) 'math '+ 43)
+((example 'insert-proc!) 'math '- 45)
+((example 'insert-proc!) 'math '* 42)
+((example 'insert-proc!) 'letters 'a 97)
+((example 'insert-proc!) 'letters 'b 98)
+
+((example 'lookup-proc) 'math '/) ;#f
+((example 'lookup-proc) 'math '-) ;45
+((example 'lookup-proc) 'letters 'b)    ;98
