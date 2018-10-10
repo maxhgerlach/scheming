2a9093832b847b1242c2e54e760d88ac038a9282
Max H. Gerlach <m@maxgerlach.de>
2018-10-10 22:06:26 +0200

    ex. 3.28

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -751,3 +751,30 @@ n
                ((= s2 1) 1)
                (else "Invalid signal s2: " s2)))
         (else (error "Invalid signal s1: " s1))))
+
+
+;; Exercise 3.28
+
+(define (or-gate o1 o2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal o1) (get-signal o2))))
+      (after-delay
+       or-gate-delay
+       (lambda () (set-signal! output new-value)))))
+  (add-action! o1 or-action-procedure)
+  (add-action! o2 or-action-procedure)
+  'ok)
+
+(define (logical-or s1 s2)
+  (cond ((= s1 0)
+         (cond ((= s2 0) 0)
+               ((= s2 1) 1)
+               (else "Invalid signal s2: " s2)))
+        ((= s1 1)
+         (cond ((= s2 0) 0)
+               ((= s2 1) 1)
+               (else "Invalid signal s2: " s2)))
+        (else (error "Invalid signal s1: " s1))))
+
+
