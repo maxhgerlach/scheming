6a92bc86a3b5cddf0f1a1077ec55550c3f156e7b
Max H. Gerlach <m@maxgerlach.de>
2018-10-10 22:04:02 +0200

    3.3.4 a simulator for digital circuits

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -699,3 +699,55 @@ n
 ;; scheme@(guile-user)> ((ex 'lookup-proc) '(stuff letters a))
 ;; $12 = 999
 
+
+
+;; Section 3.3.4 A Simulator for Digital Circuits
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire)) (c1 (make-wire)) (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! input invert-input) 'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay
+       and-gate-delay
+       (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and s1 s2)
+  (cond ((= s1 0)
+         (cond ((= s2 0) 0)
+               ((= s2 1) 0)
+               (else "Invalid signal s2: " s2)))
+        ((= s1 1)
+         (cond ((= s2 0) 0)
+               ((= s2 1) 1)
+               (else "Invalid signal s2: " s2)))
+        (else (error "Invalid signal s1: " s1))))
