1847bf9ba23bb27f06dcbcd07e1510f64c75ba98
Max H. Gerlach <m@maxgerlach.de>
2018-10-14 23:11:12 +0200

    Ex. 3.35

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -1162,7 +1162,7 @@
 ;; Probe: Celsius temp = 25
 ;; Probe: Fahrenheit temp = 77
 
-(set-value! F 212 'user)
+;(set-value! F 212 'user)
 ;; ERROR: In procedure scm-error:
 ;; Contradiction (77 212)
 
@@ -1245,3 +1245,51 @@
 
 (set-value! louis-b 99 'user)
 ;; Probe: b = 99
+
+
+;; Ex. 3.35
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0: SQUARER"
+                   (get-value b))
+            (set-value! a
+                        (sqrt (get-value b))
+                        me))
+        (if (has-value? a)
+            (set-value! b
+                        (* (get-value a) (get-value a))
+                        me))))
+  (define (process-forget-value)
+    (forget-value! b me)
+    (forget-value! a me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+          ((eq? request 'I-lost-my-value) (process-forget-value))
+          (else (error "Unknown request: SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+(define proper-a (make-connector))
+(define proper-b (make-connector))
+
+(squarer proper-a proper-b)
+
+(probe "a" proper-a)
+(probe "b" proper-b)
+
+(set-value! proper-a 9 'user)
+;; Probe: a = 9
+;; Probe: b = 81
+
+(forget-value! proper-a 'user)
+;; Probe: a = ?
+;; Probe: b = ?
+
+(set-value! proper-b 99 'user)
+;; Probe: b = 99
+;; Probe: a = 9.9498743710662
