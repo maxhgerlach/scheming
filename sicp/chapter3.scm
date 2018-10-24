face95f6775df71a531edf7ad5cb89025547bd27
Max H. Gerlach <m@maxgerlach.de>
2018-10-24 23:25:15 +0200

    3.5.1 Streams, basic example

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -1487,3 +1487,83 @@
         ((serializer1 (serializer2 exchange))
          account1
          account2)))))
+
+
+;; 3.5 Streams
+
+;; 3.5.1 Streams as Delayed Lists
+
+;; (delay <exp>) could have been syntactic sugar for (lambda ()
+;; <exp>), but really would include memoization
+
+;; force may be implemented as a procedure (define (force
+;; delayed-object) (delayed-object))
+
+;; (cons-stream <a> <b>) <=> (cons <a> (delay <b>))
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define the-empty-stream '())
+
+(define stream-null? null?)
+
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x) (display x) (newline))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter
+                       pred
+                       (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+;; (define (square x) (* x x))
+;; (define (smallest-divisor n) (find-divisor n 2))
+;; (define (find-divisor n test-divisor)
+;;   (cond ((> (square test-divisor) n) n)
+;;         ((divides? test-divisor n) test-divisor)
+;;         (else (find-divisor n (+ test-divisor 1)))))
+;; (define (divides? a b) (= (remainder b a) 0))
+
+;; (define (prime? n)
+;;   (= n (smallest-divisor n)))
+
+;; (stream-car
+;;  (stream-cdr
+;;   (stream-filter prime?
+;;                  (stream-enumerate-interval
+;;                   10000 1000000))))     ; 10009
+
