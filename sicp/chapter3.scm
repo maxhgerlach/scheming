c0b97fc7f0ad95eae482c2947ef14bc2db8a3b07
Max H. Gerlach <m@maxgerlach.de>
2018-10-28 00:34:30 +0200

    infinite streams

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -1660,3 +1660,73 @@
 ;; Without memoization the side effect would be triggered anew every
 ;; time the stream seq is iterated over.  This would surely change
 ;; results.
+
+
+;; Sec. 3.5.2 Infinite Streams
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+(define no-sevens
+  (stream-filter (lambda (x) (not (divisible? x 7)))
+                 integers))
+
+;; (stream-ref no-sevens 100)              ; 117
+
+(define (fibgen a b) (cons-stream a (fibgen b (+ a b))))
+(define fibs (fibgen 0 1))
+
+;; Sieve of Eratosthenes
+(define (sieve stream)
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter
+           (lambda (x)
+             (not (divisible? x (stream-car stream))))
+           (stream-cdr stream)))))
+(define primes (sieve (integers-starting-from 2)))
+
+;; (stream-ref primes 0)                   ;2
+;; (stream-ref primes 1)                   ;3
+;; (stream-ref primes 50)                  ;233
+;; (stream-ref primes 500)                 ;3581
+;; (stream-ref primes 2000)                ;17393
+;; (stream-ref primes 4000)                ;37831
+;; (stream-ref primes 5000)                ;48619
+;; (stream-ref primes 5050)                ;49139
+
+
+;; Defining streams implicitly
+
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define integers
+  (cons-stream 1 (add-streams ones integers)))
+
+(define fibs
+  (cons-stream
+   0
+   (cons-stream 1 (add-streams (stream-cdr fibs) fibs))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+(define double (cons-stream 1 (scale-stream double 2)))
+
+(define primes
+  (cons-stream
+   2
+   (stream-filter prime? (integers-starting-from 3))))
+
+(define (prime? n)
+  (define (iter ps)
+    (cond ((> (square (stream-car ps)) n) true)
+          ((divisible? n (stream-car ps)) false)
+          (else (iter (stream-cdr ps)))))
+  (iter primes))
+
