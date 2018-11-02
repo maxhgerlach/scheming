c7609d4509ae9a49784bd6566e34c475c35f3bf7
Max H. Gerlach <m@maxgerlach.de>
2018-11-02 21:46:03 +0100

    Ex. 3.58

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -1844,4 +1844,17 @@
 ;; This is the Fibonacci relation, solution: additions(n) = \phi ^ n
 
 
+;; Ex. 3.58
 
+(define (expand num den radix)
+  (cons-stream
+   (quotient (* num radix) den)
+   (expand (remainder (* num radix) den) den radix)))
+
+;; stream of digits represting the fraction (num/den) in base radix
+
+; (display-n 10 (expand 1 7 10))
+; => 1 4 2 8 5 7 1 4 2 8 5 ...
+
+; (display-n 10 (expand 3 8 10))
+; => 3 7 5 0 0 0 0 0 0 0 0 ...
