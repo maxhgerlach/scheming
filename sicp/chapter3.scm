563c990d03efb5e701cc9410f9338168a12390b9
Max H. Gerlach <m@maxgerlach.de>
2018-11-03 12:01:56 +0100

    Formulating iterations as stream processes

--- sicp/chapter3.scm
+++ sicp/chapter3.scm
@@ -1931,4 +1931,59 @@
 
 ;; (display-n 10 tangent-series)
 
+
+
+;; Formulating iterations as stream processes
+
+(define (average x y)
+  (/ (+ x y) 2))
      
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (square a)
+  (* a a))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream
+     1.0
+     (stream-map (lambda (guess) (sqrt-improve guess x))
+                 guesses)))
+  guesses)
+
+;; (display-n 10 (sqrt-stream 2))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+                                        ; S n −1
+        (s1 (stream-ref s 1))
+                                        ; S n
+        (s2 (stream-ref s 2)))
+                                        ; S n+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+
+;; (display-n 10 (euler-transform (sqrt-stream 2)))  ;; nan's quickly
+
+;; (display-n 10 (euler-transform pi-stream))
+
+(define (make-tableau transform s)
+  (cons-stream s (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s)))
+
+;; (display-n 10
+;;  (accelerated-sequence euler-transform pi-stream))
+
+
+;; Exercise 3.63
